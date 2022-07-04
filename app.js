65c927b91f299d50d6b919b52e4fbfb6a9072870
Manish Gupta <manish.info2020@gmail.com>
2022-07-04 21:20:44 +0530

    style

---
+++ app.js
@@ -0,0 +1,34 @@
+// // Profit = (SellingPrice - CostPrice)
+// // Profit Percentage = (Profit/CostPrice)*100
+// // Loss = (CostPrice - SellingPrice)
+// // Loss Percentage = (Loss/CostPrice)*100
+// // Challenge
+// // Create a Function calculateProfitAndLoss to calculate Loss and Profit.
+// // Use Conditional Branching to check if Loss is to be calculated or Profit.
+// // // Conditional Branching
+// // if(sellingPrice > CostPrice){
+// // 	// Profit Logic
+// // }else if(costPrice > sellingPrice){
+// // 	// Loss Logic
+// // }
+// // Add cons
+
+// function  calculateProfitAndLoss(sellingPrice,costPrice) {
+//     if(sellingPrice > costPrice) {
+//         const profit = sellingPrice - costPrice
+//         const profitPercentage = (profit/costPrice) * 100;
+//         console.log("WoW You get profit of " + profitPercentage)
+
+//     } else if (costPrice > sellingPrice) {
+//         const loss = costPrice - sellingPrice 
+//         const lossPercentage = (loss/costPrice) * 100;
+//         console.log("Oh! You got loss of " + lossPercentage)
+
+//     } else{
+//         console.log("No Gain No Pain")
+//     }
+// }
+// calculateProfitAndLoss()
+
+
+console.log(this)
